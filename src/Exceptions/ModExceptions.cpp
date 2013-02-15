4ffce7068c365b771735be4b5618a135728a09d3
jspdown <harold.ozouf@epitech.eu>
2013-02-15 16:47:07 +0100

    k

---
+++ src/Exceptions/ModExceptions.cpp
@@ -0,0 +1,51 @@
+
+#include	<sstream>
+#include	"ModExceptions.hh"
+
+CPUException::CPUException(const std::string &msg, int line)
+{
+  std::ostringstream    o;
+
+  o << "[CPU] Error line " << line << " : " << msg;
+  this->msg = o.str();
+}
+CPUException::~CPUException()	throw()
+{
+  //delete registers?
+}
+const char      *CPUException::what() const throw()
+{
+  return (this->msg.c_str());
+}
+
+MemoryException::MemoryException(const std::string &msg, int line)
+{
+  std::ostringstream    o;
+
+  o << "[Memory] Error line " << line << " : " << msg;
+  this->msg = o.str();
+}
+MemoryException::~MemoryException() throw()
+{
+  //delete registers?
+}
+const char      *MemoryException::what() const throw()
+{
+  return (this->msg.c_str());
+}
+
+OperandException::OperandException(const std::string &msg, int line)
+{
+  std::ostringstream    o;
+
+  o << "[Operand] Error line " << line << " : " << msg;
+  this->msg = o.str();
+}
+OperandException::~OperandException() throw()
+{
+  //delete registers?
+}
+const char      *OperandException::what() const throw()
+{
+  return (this->msg.c_str());
+}
---
+++ src/Exceptions/ModExceptions.hh
@@ -0,0 +1,35 @@
+
+#ifndef		__CPUEXCEPTION_H__
+# define	__CPUEXCEPTION_H__
+
+#include	<exception>
+#include	<string>
+
+class	CPUException: public	std::exception
+{
+  std::string         msg;
+public:
+  CPUException(const std::string &, int line);
+  virtual ~CPUException() throw();
+  virtual const char  *what() const throw();
+};
+
+class	MemoryException: public	std::exception
+{
+  std::string         msg;
+public:
+  MemoryException(const std::string &, int line);
+  virtual ~MemoryException() throw();
+  virtual const char  *what() const throw();
+};
+
+class	OperandException: public	std::exception
+{
+  std::string         msg;
+public:
+  OperandException(const std::string &, int line);
+  virtual ~OperandException() throw();
+  virtual const char  *what() const throw();
+};
+
+#endif
